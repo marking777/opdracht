a8418f8039b78daf5fb7e2f91702e27071f40b5c
Marouane el Jaghnouni <2179336@talnet.nl>
2023-05-15 09:57:53 +0200

    commit

---
+++ opd2.php
@@ -0,0 +1,21 @@
+<?php
+
+$variabele1 = 10;
+$variabele2 = 10;
+
+if($variabele1 == $variabele2) {
+    echo "De twee waarden zijn gelijk";
+}
+
+if($variabele1 !== $variabele2) {
+    echo "De twee waarden zijn gelijk";
+}
+$variabele1 = 10;
+$variabele2 = 10;
+
+if($variabele1 == $variabele2)
+    echo "De twee waarden zijn gelijk";
+else
+    echo "De twee waarden zijn ongelijk";
+    
+?>
